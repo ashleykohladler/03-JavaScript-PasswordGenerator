f978a6fe2f7219b4a6a4b845dc0d0bbbeb43724f
Ashley Kohl <akohl22@gmail.com>
2021-03-07 10:14:03 -0700

    updated read me

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Define variables
 var generateBtn = document.querySelector("#generate");
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -10,18 +10,18 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 
-//generate password
+// function to generate password
 function generatePassword() {
 
   var userLength = parseInt(prompt('How many characters? [8-128]'));
   
-
-  if ( isNaN(userLength) || userLength < 8 || userLength > 128) {
+//define length of password
+  if ( Number.isNaN(userLength) || userLength < 8 || userLength > 128) {
     alert("Length must be between 8 and 128")
     return
   
   }
-  
+//define which characters are allowed
   var allowedChars = [];
 
   var useLowerCase = prompt('Do you want lowercase letters? (Y/N)').toUpperCase();
@@ -53,6 +53,7 @@ function generatePassword() {
     return allowedChars;
   }
  
+  //return password
   var password = "";
 
   for(var i=0; i < userLength; i++) {
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# 03-JavaScript-PasswordGenerator
+# 03-JavaScritpt-PasswordGenerator
+
+# Homework 3
+
+## Description of my work 
+
+**JavaScript**
+
+* I defined my variables.
+
+* I created a function to generate password.
+
+* I created an if statement to define length of password to be between 8-128 characters.
+
+* I define a variable for which characters are allowed.
+
+* I created a function to write the password.
+
+* Added an event listener to generate button.
+
+## Link to Website
+
+
+## Screenshot
+![Screenshot](Develop/image/screenshot.jpg)
